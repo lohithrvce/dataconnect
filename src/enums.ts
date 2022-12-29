13b86fe53a215b5394e5001d3268ee6e5a8fb108
Lohith R <lohith.r@tredence.com>
2022-12-29 15:27:46 +0530

    adding all code for data connect

---
+++ src/enums.ts
@@ -0,0 +1,4 @@
+export const EXTENSION_ID = 'dataconnectornode-jupyterlab'
+export const EXTENSION_NAME = 'Data Connector'
+export const OPEN_COMMAND = 'jle:open'
+export const EXTENSION_CSS_CLASSNAME = 'jl-ReactAppWidget'
