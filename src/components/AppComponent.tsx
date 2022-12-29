50b5146d5fd92af5fcd6029379186a94e2f4e37f
Lohith R <lohith.r@tredence.com>
2022-12-29 17:48:29 +0530

    css changed

--- src/components/AppComponent.tsx
+++ src/components/AppComponent.tsx
@@ -1,32 +1,37 @@
 import React from 'react';
 import { EXTENSION_CSS_CLASSNAME } from '../enums';
 
+
 export const AppComponent = (): JSX.Element => {
   return (
     <div className={EXTENSION_CSS_CLASSNAME}>
       <div className="data-connector-form">
-        <div>ADD NEW CONNECTOR</div>
-        <div>
+        <div className='Header'>ADD NEW CONNECTOR</div>
+        <div className='SpanSelect'>
           <span>Please select the connector</span>
           <select>
             <option>Postgres</option>
             <option>MySql</option>
           </select>
         </div>
-        <div>
+        <div className='SpanInput'>
           <span>Host: </span>
           <input />
         </div>
-        <div>
+        <div className='SpanInput'>
           <span>Username: </span>
           <input />
         </div>
-        <div>
+        <div className='SpanInput'>
           <span>Password: </span>
           <input />
         </div>
-        <div>
+        <div className='SpanInput'>
           <span>Database: </span>
+          <input />
+        </div>
+        <div className='SpanInput'>
+          <span>Schema: </span>
           <input />
         </div>
       </div>
